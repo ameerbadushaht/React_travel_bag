1544713fd7d5ac91d9a81299f03ae7a8ca39d080
Ameer Badushah T <84665215+ameerbadushaht@users.noreply.github.com>
2023-10-08 13:20:49 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import Header from './components/Header'
+import Form from './components/Form'
+import PackageList from './components/PackageList'
+import Stats from './components/Stats'
+import { useState } from "react";
+function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
+  function handleClearList() {
+    const confirmed = window.confirm(
+      "Are you sure you want to delete all items?"
+    );
+
+    if (confirmed) setItems([]);
+  }
+  return (
+    <div className="app">
+    <Header />
+    <Form onAddItems={handleAddItems} />
+    <PackageList
+      items={items}
+      onDeleteItem={handleDeleteItem}
+      onToggleItem={handleToggleItem}
+      onClearList={handleClearList}
+    />
+    <Stats items={items} />
+  </div>
+  )
+}
+
+export default App
